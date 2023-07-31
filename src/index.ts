188ecae0a2a992264eefc807725e74d5e3180de5
adarsh-shahi <adarshshahi1009@gmail.com>
2023-07-31 23:08:28 +0530

    limited response data properties

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello there");
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import authRouter from "./routers/auth";
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use("/auth", authRouter);
+
+app.listen(PORT, () => {
+	console.log(`Listening on ${PORT}`);
+});
